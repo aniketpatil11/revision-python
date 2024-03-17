10e688d1d726d261dfb68762389a3b63ec357111
aniketpatil11 <aniket112.patil@gmail.com>
2024-03-17 22:18:12 +0530

    Adds first oop exercise

---
+++ basic-oop/exercise1_class.py
@@ -0,0 +1,20 @@
+# Exercise 1: 
+
+class Dog:
+    species = "Canis familiaris"
+    def __init__(self, name, age, coat_color):
+        self.name = name
+        self.age = age
+        self.coat_color = coat_color
+# Instance method
+    def description(self):
+        return f"{self.name} is {self.age} years old"
+# Another instance method
+    def speak(self, sound):
+        return f"{self.name} says {sound}"
+    
+lab = Dog("Rocky", 2, "White")
+print(lab.name)
+print(lab.species)
+philo = Dog("Philo", 5, "brown")
+print(f"{philo.name}'s coat is {philo.coat_color}")
