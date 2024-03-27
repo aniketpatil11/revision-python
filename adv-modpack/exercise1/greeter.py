85cd010ec82158af6eb5f9f84625255514079457
aniketpatil11 <aniket112.patil@gmail.com>
2024-03-27 13:41:40 +0530

    Adds Module exercises 1

---
+++ adv-modpack/exercise1/greeter.py
@@ -0,0 +1,4 @@
+# Greeter function is defined here
+
+def greet(name): 
+    return f"Hello {name}"
---
+++ adv-modpack/exercise1/main.py
@@ -0,0 +1,5 @@
+# Import greeter : greet
+from greeter import greet
+
+greet_value = greet("Real Python")
+print(greet_value)
